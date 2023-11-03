f07d4f58fb96d209fb41afd0e90d7c52e7dcac5f
sui <1202ayasnppk@gmail.com>
2023-11-03 13:27:53 +0900

    add: footer, Header

---
+++ src/app/Header.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='py-5 px-10 border-b flex justify-between items-center'>
+      <div>
+        <h1 className='text-2xl font-sans'>
+          <Link href={'/'}>Next.js 13 Blog</Link>
+        </h1>
+      </div>
+      <nav className='text-sm font-medium'>
+        <Link href={'/articles/new'} className='bg-orange-400 px-3 py-3 rounded-md'>
+          記事を書く
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
